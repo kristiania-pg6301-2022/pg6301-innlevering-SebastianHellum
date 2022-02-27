3dcfcdfac7eae535f03a26d8827caf7567ae7f6e
Sebastian Hellum <sebhellum@gmail.com>
2022-02-27 15:00:46 +0100

    GET & POST request in API without fetching data

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 import express from "express";
 import * as path from "path";
+import { isCorrectAnswer, randomQuestion, Questions } from "./quiz.js";
 
 const app = express();
 
-app.use(express.static("../client/dist"));
-app.use((req, res, next) => {
-  if (req.method === "GET" && !req.path.startsWith("/api/question/")) {
-    return res.sendFile(path.resolve("../client/quizGame.jsx"));
+//GET - Returns random question with: id, category, question, answers
+app.get("/api/question", (req, res) => {
+  const { id, category, question, answers } = randomQuestion();
+  res.json({ id, category, question, answers });
+});
+
+//POST - Takes in:  id, answer, Returns: "true" || "false"
+app.post("/api/question", (req, res) => {
+  const { id, answer } = req.body;
+  const question = Questions.find((q) => q.id === id);
+  if (!question) {
+    return res.sendStatus(404);
+  }
+  if (isCorrectAnswer({ question, answer })) {
+    return res.json({ result: "true" });
   } else {
-    next();
+    return res.json({ result: "false" });
   }
 });
 
@@ -21,6 +33,6 @@ app.use((req, res, next) => {
   }
 });
 
-const server = app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
   console.log(`Server started on http://localhost:${server.address().port}`);
 });

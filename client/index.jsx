ddb5c506c117c87f0e01d46d6803c2739f3b500f
Sebastian Hellum <sebhellum@gmail.com>
2022-02-27 12:13:12 +0100

    QuizGame working in client without API

--- client/index.jsx
+++ client/index.jsx
@@ -1,37 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { QuizGame } from "./quizGame";
 
-function Frontpage() {
-  return (
-    <div>
-      <h1>Quiz time</h1>
-      <div>
-        <div>
-          <Link to={"/quiz"}>Start quiz</Link>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function Quiz() {
-  return (
-    <div>
-      <h1>It's quiz time</h1>
-    </div>
-  );
-}
-
-function Application() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Frontpage />} />
-        <Route path="/quiz" element={<Quiz />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-ReactDOM.render(<Application />, document.getElementById("app"));
+ReactDOM.render(<QuizGame />, document.getElementById("app"));
